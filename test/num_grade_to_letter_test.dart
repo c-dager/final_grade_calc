589b6e93ada9753f6559e04a5e33f7330ef021e3
c-dager <dagerc@protonmail.com>
2024-09-02 19:30:35 -0400

    Refactor tests to reduce duplication

--- test/num_grade_to_letter_test.dart
+++ test/num_grade_to_letter_test.dart
@@ -1,22 +1,16 @@
+import 'dart:collection';
+
 import 'package:final_grade_calc/num_grade_to_letter.dart';
 import 'package:test/test.dart';
 
 void main() {
   final numToLetterConverter = new numToLetterGrade();
-  test("95 is letter grade A", () {
-    String result = numToLetterConverter.getLetterGrade(95);
-    expect("A", result);
-  });
-  test("85 is letter grade B", () {
-    String result = numToLetterConverter.getLetterGrade(85);
-    expect("B", result);
-  });
-  test("75 is letter grade C", () {
-    String result = numToLetterConverter.getLetterGrade(75);
-    expect("C", result);
-  });
-  test("65 is letter grade D", () {
-    String result = numToLetterConverter.getLetterGrade(65);
-    expect("D", result);
-  });
+  final Map<int, String> numberAndMatchingLetterGradeList = {95: 'A', 85: 'B', 75: 'C', 65: 'D', 55: 'F'};
+
+  for(MapEntry numberAndLetterGradeCombo in numberAndMatchingLetterGradeList.entries){
+    test("${numberAndLetterGradeCombo.key} is letter grade ${numberAndLetterGradeCombo.value}", () {
+      String result = numToLetterConverter.getLetterGrade(numberAndLetterGradeCombo.key);
+      expect(numberAndLetterGradeCombo.value, result);
+    });
+  }
 }

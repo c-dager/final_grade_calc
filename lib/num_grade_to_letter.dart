c5c43742fac0f4560493b58d429abd5f84f9b87b
c-dager <dagerc@protonmail.com>
2024-09-02 18:45:34 -0400

    Added checkIfAGrade function

--- lib/num_grade_to_letter.dart
+++ lib/num_grade_to_letter.dart
@@ -1,5 +1,18 @@
 class numToLetterGrade{
   String getLetterGrade(int numGrade){
-    return "A";
+    if(checkIfAGrade(numGrade)){
+      return "A";
+    }
+    else{
+      return "";
+    }
+  }
+  bool checkIfAGrade(numGrade){
+    if(numGrade >= 90){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 }
